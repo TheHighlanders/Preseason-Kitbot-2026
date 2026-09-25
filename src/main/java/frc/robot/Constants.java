73d2e6e404e96cbe57b3d0b8b30d60b410de352f
lessfood <emelina.cheung@gmail.com>
2026-09-25 16:24:48 -0400

    drive train and drive commands

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,4 +16,11 @@ public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
   }
+  public static class DriveConstants {
+    public static final int LEFT_LEADER_ID = 0;
+    public static final int RIGHT_LEADER_ID = 0;
+    public static final int LEFT_FOLLOWER_ID = 0;
+    public static final int RIGHT_FOLLOWER_ID = 0;
+  }
+ 
 }
